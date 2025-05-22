ccea71e06e7e0615fefe27fa084d8693294aab1a
tia-lab <tia@eki.studio>
2025-05-22 20:17:41 +0200

    [43] fix: add .js extension to schema imports in index.ts

--- packages/zod-sql/src/zod-sql/index.ts
+++ packages/zod-sql/src/zod-sql/index.ts
@@ -1,8 +1,8 @@
-export { extractTableSchema, extractTableStructure } from './schema/index'
+export { extractTableSchema, extractTableStructure } from './schema/index.js'
 export type {
 	AutoFieldsType,
 	ColumnDefinition,
 	FlattenedSchemaType,
 	TableStructure
-} from './schema/index'
-export * from './tables/index'
+} from './schema/index.js'
+export { createTable, createTableDDL } from './tables/index.js'
